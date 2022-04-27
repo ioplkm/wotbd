f344db0d20a487a1bf89b5e7772d0eeae6f9312d
ioplkm <ioplkm352@gmail.com>
2022-04-27 23:41:11 +0500

    reduced network usage

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
-CC=clang
+CC = clang
+CFLAGS = -std=c99 -O3 -Wall -Wextra -Wpedantic
+LDFLAGS = -lcurl -ljson-c
 all: main.c
-	$(CC) main.c -lcurl -ljson-c -Wall -std=c99 -o wotb -O3
+	$(CC) $(CFLAGS) $(LDFLAGS) main.c -o wotb
 install:
 	cp wotb /usr/local/bin
 	chmod +x /usr/local/bin/wotb
+clean:
+	rm wotb
--- main.c
+++ main.c
@@ -1,62 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 #include <unistd.h>
+
 #include <curl/curl.h>
 #include <json-c/json.h>
 
 struct stats {
-  int damage_dealt;
-  int shots;
-  int hits;
-  int wins;
-  int battles;
-  int survived_battles;
+  uint16_t damage_dealt;
+  uint8_t shots;
+  uint8_t hits;
+  uint8_t wins;
+  uint8_t battles;
+  uint8_t survived_battles;
 };
 
-//const char* tank_id = "3681";
-//const char* tank_id = "19537";
-const char* account_id = "126219745";
-const char* app_id = "74198832ec124e1cfe22490f35a7085f";
+const char* accountId = "126219745";
+const char* appId = "74198832ec124e1cfe22490f35a7085f";
 
-int last_battle_time;
-
-int counter;
+time_t lastBattleTime;
+bool isTimeChanged;
 
 struct stats initial;
-struct stats current;
 struct stats last;
 
-size_t json_parse(void* b, size_t size, size_t nmemb, void *userp);
+size_t initParse(void* b, size_t size, size_t nmemb);
+size_t timeParse(void* b, size_t size, size_t nmemb);
+size_t dataParse(void* b, size_t size, size_t nmemb);
 
 int main(int argc, char* argv[]) {
   curl_global_init(CURL_GLOBAL_ALL);
-  CURL *handle = curl_easy_init();
   char *url = (char*)malloc(sizeof(char) * 999); 
-  //sprintf(url, "https://api.wotblitz.ru/wotb/account/tankstats/?application_id=%s&account_id=%s&tank_id=%s", app_id, account_id, tank_id);
-  sprintf(url, "https://api.wotblitz.ru/wotb/account/info/?application_id=%s&account_id=%s", app_id, account_id);
+
+  CURL *handle = curl_easy_init();
+  sprintf(url, "https://api.wotblitz.ru/wotb/account/info/?application_id=%s&account_id=%s&fields=last_battle_time%%2Cstatistics.all.battles%%2Cstatistics.all.damage_dealt%%2Cstatistics.all.wins%%2Cstatistics.all.survived_battles%%2Cstatistics.all.hits%%2Cstatistics.all.shots", appId, accountId);
   curl_easy_setopt(handle, CURLOPT_URL, url);
+  curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, initParse);
+  curl_easy_perform(handle);
+
+  CURL *timeHandle = curl_easy_init();
+  sprintf(url, "https://api.wotblitz.ru/wotb/account/info/?application_id=%s&account_id=%s&fields=last_battle_time", appId, accountId);
+  curl_easy_setopt(timeHandle, CURLOPT_URL, url);
+  curl_easy_setopt(timeHandle, CURLOPT_WRITEFUNCTION, timeParse);
+
+  sprintf(url, "https://api.wotblitz.ru/wotb/account/info/?application_id=%s&account_id=%s&fields=statistics.all.battles%%2Cstatistics.all.damage_dealt%%2Cstatistics.all.wins%%2Cstatistics.all.survived_battles%%2Cstatistics.all.hits%%2Cstatistics.all.shots", appId, accountId);
+  curl_easy_setopt(handle, CURLOPT_URL, url);
+  curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, dataParse);
+
   free(url);
-  curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, json_parse);
   printf("\033c");
   printf("num |  damage | hitrate | winrate | survived\n");
   for (;;) {
-    curl_easy_perform(handle);
+    curl_easy_perform(timeHandle);
+    if (isTimeChanged) {
+      curl_easy_perform(handle);
+    }
     sleep(1);
   }
   curl_global_cleanup();
   return 0;
 }
 
-size_t json_parse(void *buffer, size_t size, size_t nmemb, void *userp) {
+size_t initParse(void *buffer, size_t size, size_t nmemb) {
+  json_object *obj = json_tokener_parse(buffer);
+  json_object *data, *me, *last_battle_time, *all, *statistics;
+  json_object_object_get_ex(obj, "data", &data);
+  json_object_object_get_ex(data, accountId, &me);
+  json_object_object_get_ex(me, "last_battle_time", &last_battle_time);
+  json_object_object_get_ex(me, "statistics", &statistics);
+  json_object_object_get_ex(statistics, "all", &all);
+  json_object *damage_dealt, *shots, *hits, *battles, *survived_battles, *wins;
+  json_object_object_get_ex(all, "damage_dealt", &damage_dealt);
+  json_object_object_get_ex(all, "shots", &shots);
+  json_object_object_get_ex(all, "hits", &hits);
+  json_object_object_get_ex(all, "wins", &wins);
+  json_object_object_get_ex(all, "battles", &battles);
+  json_object_object_get_ex(all, "survived_battles", &survived_battles);
+  initial.damage_dealt = json_object_get_int(damage_dealt);
+  initial.shots = json_object_get_int(shots);
+  initial.hits = json_object_get_int(hits);
+  initial.wins = json_object_get_int(wins);
+  initial.battles = json_object_get_int(battles);
+  initial.survived_battles = json_object_get_int(survived_battles);
+  last = initial;
+  lastBattleTime = json_object_get_int(last_battle_time);
+  return size * nmemb;
+}
+
+size_t timeParse(void *buffer, size_t size, size_t nmemb) {
+  json_object *obj = json_tokener_parse(buffer);
+  json_object *data, *me, *last_battle_time;
+  json_object_object_get_ex(obj, "data", &data);
+  json_object_object_get_ex(data, accountId, &me);
+  json_object_object_get_ex(me, "last_battle_time", &last_battle_time);
+  isTimeChanged = json_object_get_int(last_battle_time) != lastBattleTime;
+  if (isTimeChanged) lastBattleTime = json_object_get_int(last_battle_time);
+  return size * nmemb;
+}
+
+size_t dataParse(void *buffer, size_t size, size_t nmemb) {
   struct json_object *data;
   struct json_object *me;
-  struct json_object *lbt;
   json_object *obj = json_tokener_parse(buffer);
   json_object_object_get_ex(obj, "data", &data);
-  json_object_object_get_ex(data, account_id, &me);
-  json_object_object_get_ex(me, "last_battle_time", &lbt);
-  if (json_object_get_int(lbt) != last_battle_time) {
-    //printf("time is updated\n");
+  json_object_object_get_ex(data, accountId, &me);
     struct json_object *all;
     struct json_object *statistics;
     struct json_object *damage_dealt;
@@ -73,16 +121,6 @@ size_t json_parse(void *buffer, size_t size, size_t nmemb, void *userp) {
     json_object_object_get_ex(all, "wins", &wins);
     json_object_object_get_ex(all, "battles", &battles);
     json_object_object_get_ex(all, "survived_battles", &survived_battles);
-    if (!last_battle_time) {
-      initial.damage_dealt = json_object_get_int(damage_dealt);
-      initial.shots = json_object_get_int(shots);
-      initial.hits = json_object_get_int(hits);
-      initial.wins = json_object_get_int(wins);
-      initial.battles = json_object_get_int(battles);
-      initial.survived_battles = json_object_get_int(survived_battles);
-      last = initial;
-    } else {
-      counter++;
       struct stats current = {.damage_dealt = json_object_get_int(damage_dealt),
                               .hits = json_object_get_int(hits),
                               .shots = json_object_get_int(shots),
@@ -97,7 +135,7 @@ size_t json_parse(void *buffer, size_t size, size_t nmemb, void *userp) {
 			      .survived_battles = current.survived_battles - last.survived_battles};
       printf("\033[2K\r");
       printf("%3i | %7.2f | %6.2f%% | %7s | %8s\n", 
-             counter,
+             current.battles - initial.battles,
              (float)battle.damage_dealt / battle.battles, 
 	     (float)battle.hits / battle.shots * 100, 
 	     battle.wins ? "win" : "lose", 
@@ -115,8 +153,5 @@ size_t json_parse(void *buffer, size_t size, size_t nmemb, void *userp) {
 	     (float)session.survived_battles / session.battles * 100);
       fflush(stdout);
       last = current;
-    }
-    last_battle_time = json_object_get_int(lbt);
-  }
   return size * nmemb;
 }
