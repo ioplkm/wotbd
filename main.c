476363e7282ff0a4317ad9cc45572e609843a3ab
ioplkm <ioplkm352@gmail.com>
2022-04-28 03:03:27 +0500

    added frags and spots

--- main.c
+++ main.c
@@ -8,12 +8,15 @@
 #include <json-c/json.h>
 
 struct stats {
-  uint16_t damage_dealt;
+  uint16_t damageDealt;
   uint8_t shots;
   uint8_t hits;
   uint8_t wins;
+  uint8_t losses;
   uint8_t battles;
-  uint8_t survived_battles;
+  uint8_t survivedBattles;
+  uint8_t frags;
+  uint8_t spots;
 };
 
 const char* accountId = "126219745";
@@ -34,7 +37,7 @@ int main(int argc, char* argv[]) {
   char *url = (char*)malloc(sizeof(char) * 999); 
 
   CURL *handle = curl_easy_init();
-  sprintf(url, "https://api.wotblitz.ru/wotb/account/info/?application_id=%s&account_id=%s&fields=last_battle_time%%2Cstatistics.all.battles%%2Cstatistics.all.damage_dealt%%2Cstatistics.all.wins%%2Cstatistics.all.survived_battles%%2Cstatistics.all.hits%%2Cstatistics.all.shots", appId, accountId);
+  sprintf(url, "https://api.wotblitz.ru/wotb/account/info/?application_id=%s&account_id=%s&fields=last_battle_time%%2Cstatistics.all.battles%%2Cstatistics.all.damage_dealt%%2Cstatistics.all.wins%%2Cstatistics.all.survived_battles%%2Cstatistics.all.hits%%2Cstatistics.all.shots%%2Cstatistics.all.losses%%2Cstatistics.all.frags%%2Cstatistics.all.spotted", appId, accountId);
   curl_easy_setopt(handle, CURLOPT_URL, url);
   curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, initParse);
   curl_easy_perform(handle);
@@ -44,13 +47,13 @@ int main(int argc, char* argv[]) {
   curl_easy_setopt(timeHandle, CURLOPT_URL, url);
   curl_easy_setopt(timeHandle, CURLOPT_WRITEFUNCTION, timeParse);
 
-  sprintf(url, "https://api.wotblitz.ru/wotb/account/info/?application_id=%s&account_id=%s&fields=statistics.all.battles%%2Cstatistics.all.damage_dealt%%2Cstatistics.all.wins%%2Cstatistics.all.survived_battles%%2Cstatistics.all.hits%%2Cstatistics.all.shots", appId, accountId);
+  sprintf(url, "https://api.wotblitz.ru/wotb/account/info/?application_id=%s&account_id=%s&fields=statistics.all.battles%%2Cstatistics.all.damage_dealt%%2Cstatistics.all.wins%%2Cstatistics.all.survived_battles%%2Cstatistics.all.hits%%2Cstatistics.all.shots%%2Cstatistics.all.losses%%2Cstatistics.all.frags%%2Cstatistics.all.spotted", appId, accountId);
   curl_easy_setopt(handle, CURLOPT_URL, url);
   curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, dataParse);
 
   free(url);
   printf("\033c");
-  printf("num |  damage | hitrate | winrate | survived\n");
+  printf(" num | damage | hitrate | winrate | survival | frags | spots\n");
   for (;;) {
     curl_easy_perform(timeHandle);
     if (isTimeChanged) {
@@ -70,20 +73,26 @@ size_t initParse(void *buffer, size_t size, size_t nmemb) {
   json_object_object_get_ex(me, "last_battle_time", &last_battle_time);
   json_object_object_get_ex(me, "statistics", &statistics);
   json_object_object_get_ex(statistics, "all", &all);
-  json_object *damage_dealt, *shots, *hits, *battles, *survived_battles, *wins;
+  json_object *damage_dealt, *shots, *hits, *battles, *survived_battles, *wins, *losses, *frags, *spotted;
   json_object_object_get_ex(all, "damage_dealt", &damage_dealt);
   json_object_object_get_ex(all, "shots", &shots);
   json_object_object_get_ex(all, "hits", &hits);
   json_object_object_get_ex(all, "wins", &wins);
   json_object_object_get_ex(all, "battles", &battles);
   json_object_object_get_ex(all, "survived_battles", &survived_battles);
-  initial.damage_dealt = json_object_get_int(damage_dealt);
+  json_object_object_get_ex(all, "losses", &losses);
+  json_object_object_get_ex(all, "frags", &frags);
+  json_object_object_get_ex(all, "spotted", &spotted);
+  initial.damageDealt = json_object_get_int(damage_dealt);
   initial.shots = json_object_get_int(shots);
   initial.hits = json_object_get_int(hits);
   initial.wins = json_object_get_int(wins);
   initial.battles = json_object_get_int(battles);
-  initial.survived_battles = json_object_get_int(survived_battles);
+  initial.survivedBattles = json_object_get_int(survived_battles);
+  initial.frags = json_object_get_int(frags);
+  initial.spots = json_object_get_int(spotted);
   last = initial;
+  last.losses = json_object_get_int(losses);
   lastBattleTime = json_object_get_int(last_battle_time);
   return size * nmemb;
 }
@@ -100,57 +109,64 @@ size_t timeParse(void *buffer, size_t size, size_t nmemb) {
 }
 
 size_t dataParse(void *buffer, size_t size, size_t nmemb) {
-  struct json_object *data;
-  struct json_object *me;
   json_object *obj = json_tokener_parse(buffer);
+  json_object *data, *me, *statistics, *all;
   json_object_object_get_ex(obj, "data", &data);
   json_object_object_get_ex(data, accountId, &me);
-    struct json_object *all;
-    struct json_object *statistics;
-    struct json_object *damage_dealt;
-    struct json_object *shots;
-    struct json_object *hits;
-    struct json_object *battles;
-    struct json_object *survived_battles;
-    struct json_object *wins;
-    json_object_object_get_ex(me, "statistics", &statistics);
-    json_object_object_get_ex(statistics, "all", &all);
-    json_object_object_get_ex(all, "damage_dealt", &damage_dealt);
-    json_object_object_get_ex(all, "shots", &shots);
-    json_object_object_get_ex(all, "hits", &hits);
-    json_object_object_get_ex(all, "wins", &wins);
-    json_object_object_get_ex(all, "battles", &battles);
-    json_object_object_get_ex(all, "survived_battles", &survived_battles);
-      struct stats current = {.damage_dealt = json_object_get_int(damage_dealt),
+  json_object_object_get_ex(me, "statistics", &statistics);
+  json_object_object_get_ex(statistics, "all", &all);
+  json_object *damage_dealt, *shots, *hits, *battles, *survived_battles, *wins, *losses, *frags, *spotted;
+  json_object_object_get_ex(all, "damage_dealt", &damage_dealt);
+  json_object_object_get_ex(all, "shots", &shots);
+  json_object_object_get_ex(all, "hits", &hits);
+  json_object_object_get_ex(all, "wins", &wins);
+  json_object_object_get_ex(all, "battles", &battles);
+  json_object_object_get_ex(all, "survived_battles", &survived_battles);
+  json_object_object_get_ex(all, "losses", &losses);
+  json_object_object_get_ex(all, "frags", &frags);
+  json_object_object_get_ex(all, "spotted", &spotted);
+      struct stats current = {.damageDealt = json_object_get_int(damage_dealt),
                               .hits = json_object_get_int(hits),
                               .shots = json_object_get_int(shots),
                               .wins = json_object_get_int(wins),
+                              .losses = json_object_get_int(losses),
                               .battles = json_object_get_int(battles),
-                              .survived_battles = json_object_get_int(survived_battles)};
-      struct stats battle = {.damage_dealt = current.damage_dealt - last.damage_dealt,
+                              .survivedBattles = json_object_get_int(survived_battles),
+                              .frags = json_object_get_int(frags),
+                              .spots = json_object_get_int(spotted)};
+      struct stats battle = {.damageDealt = current.damageDealt - last.damageDealt,
                               .hits = current.hits - last.hits,
 			      .shots = current.shots - last.shots,
 			      .wins = current.wins - last.wins,
+                              .losses = current.losses - last.losses,
 			      .battles = current.battles - last.battles,
-			      .survived_battles = current.survived_battles - last.survived_battles};
+			      .survivedBattles = current.survivedBattles - last.survivedBattles,
+                              .frags = current.frags - last.frags,
+                              .spots = current.spots - last.spots};
       printf("\033[2K\r");
-      printf("%3i | %7.2f | %6.2f%% | %7s | %8s\n", 
+      printf("%4i | %6i | %6.2f%% | %7s | %8s | %5d | %5d\n", 
              current.battles - initial.battles,
-             (float)battle.damage_dealt / battle.battles, 
+             battle.damageDealt, 
 	     (float)battle.hits / battle.shots * 100, 
-	     battle.wins ? "win" : "lose", 
-	     battle.survived_battles ? "survived" : "died");
-      struct stats session = {.damage_dealt = current.damage_dealt - initial.damage_dealt,
+	     battle.wins ? "win" : battle.losses ? "lose" : "draw", 
+	     battle.survivedBattles ? "survived" : "died",
+             battle.frags,
+             battle.spots);
+      struct stats session = {.damageDealt = current.damageDealt - initial.damageDealt,
                               .hits = current.hits - initial.hits,
 			      .shots = current.shots - initial.shots,
 			      .wins = current.wins - initial.wins,
 			      .battles = current.battles - initial.battles,
-			      .survived_battles = current.survived_battles - initial.survived_battles};
-      printf("avg | %7.2f | %6.2f%% | %6.2f%% | %7.2f%%", 
-             (float)session.damage_dealt / session.battles, 
+			      .survivedBattles = current.survivedBattles - initial.survivedBattles,
+                              .frags = current.frags - initial.frags,
+                              .spots = current.spots - initial.spots};
+      printf(" avg |%7.1f | %6.2f%% | %6.2f%% | %7.2f%% | %5.2f | %5.2f", 
+             (float)session.damageDealt / session.battles, 
 	     (float)session.hits / session.shots * 100, 
 	     (float)session.wins / session.battles * 100, 
-	     (float)session.survived_battles / session.battles * 100);
+	     (float)session.survivedBattles / session.battles * 100,
+             (float)session.frags / session.battles,
+             (float)session.spots / session.battles);
       fflush(stdout);
       last = current;
   return size * nmemb;
